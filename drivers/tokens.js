199d532be3c09e65c85e38b1b42b664aa018fe78
Stella Sajcic <sajcic.stella@gmail.com>
2019-12-03 14:08:52 +0100

    feat: tokens.net

---
+++ drivers/tokens.js
@@ -0,0 +1,28 @@
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils');
+
+module.exports = async () => {
+  const currencies = await request('https://api.tokens.net/public/trading-pairs/get/all/');
+  const data = await request('https://api.tokens.net/public/ticker/all/');
+  const markets = Object.keys(data).filter((item) => item !== 'timestamp' && item !== 'status');
+  const pairs = Object.values(markets);
+
+  return pairs.map((pair) => {
+    const ticker = data[pair];
+
+    return new Ticker({
+      base: currencies[pair] ? currencies[pair].baseCurrency : '',
+      quote: currencies[pair] ? currencies[pair].counterCurrency : '',
+      open: parseToFloat(ticker.open),
+      high: parseToFloat(ticker.high),
+      low: parseToFloat(ticker.low),
+      close: parseToFloat(ticker.last),
+      bid: parseToFloat(ticker.bid),
+      ask: parseToFloat(ticker.ask),
+      vwap: parseToFloat(ticker.vwap),
+      baseVolume: parseToFloat(ticker[`volume_${ticker.counterCurrency}`]),
+      quoteVolume: parseToFloat(ticker.volume),
+    });
+  });
+};
