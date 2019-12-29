d87c543dce73e0184b09f02f2b3f1f4a8b422b37
Mr00HasH <51995923+Mr00HasH@users.noreply.github.com>
2019-12-29 19:07:32 +0100

    Update unnamedex.js
    Switched quoteVolume & baseVolume

--- drivers/unnamedex.js
+++ drivers/unnamedex.js
@@ -13,8 +13,8 @@ module.exports = async () => {
       return new Ticker({
         base,
         quote,
-        quoteVolume: parseToFloat(ticker.volume),
-        baseVolume: parseToFloat(ticker.baseVolume),
+        quoteVolume: parseToFloat(ticker.baseVolume),
+        baseVolume: parseToFloat(ticker.volume),
         close: parseToFloat(ticker.close),
         high: parseToFloat(ticker.high),
         low: parseToFloat(ticker.low),
