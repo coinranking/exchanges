a8e73f6c5042de6e24f272670f9a05eb095b6069
Mr00HasH <51995923+Mr00HasH@users.noreply.github.com>
2019-12-29 23:48:50 +0100

    Update unnamedex.js

--- drivers/unnamedex.js
+++ drivers/unnamedex.js
@@ -6,16 +6,16 @@ module.exports = async () => {
   const { result: tickers } = await request('https://api.unnamed.exchange/v1/Public/Ticker');
 
   return tickers.map((ticker) => {
-      const [base, quote] = ticker.market.split('_');
+    const [base, quote] = ticker.market.split('_');
 
-      return new Ticker({
-        base,
-        quote,
-        quoteVolume: parseToFloat(ticker.baseVolume),
-        baseVolume: parseToFloat(ticker.volume),
-        close: parseToFloat(ticker.close),
-        high: parseToFloat(ticker.high),
-        low: parseToFloat(ticker.low),
-      });
+    return new Ticker({
+      base,
+      quote,
+      quoteVolume: parseToFloat(ticker.baseVolume),
+      baseVolume: parseToFloat(ticker.volume),
+      close: parseToFloat(ticker.close),
+      high: parseToFloat(ticker.high),
+      low: parseToFloat(ticker.low),
     });
+  });
 };
