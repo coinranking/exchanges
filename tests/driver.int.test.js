cdb85b08f6af6d4f289278219b5dd91f2dd15ccf
nickpater <nick@coinranking.com>
2019-12-30 14:11:52 +0100

    feat(tests): add test to check index.js
    This checks if all the drivers are required in the index.js and checks if this is in alphabetical
    order

--- tests/driver.int.test.js
+++ tests/driver.int.test.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const { nock, defaultOptions } = require('./helpers/nock.js');
 const drivers = require('../drivers');
 
 jest.setTimeout(60 * 1000); // Same timeout as Lambda
 
 const driverNames = Object.keys(drivers);
+const driversDir = path.join(__dirname, '..', 'drivers');
 
 const getTickers = async (driverName) => {
   const driver = drivers[driverName];
@@ -21,6 +24,17 @@ const getTickers = async (driverName) => {
 
   return tickers;
 };
+
+test('All drivers are added to the index.js file in alphabetical order', () => {
+  const files = fs.readdirSync(driversDir);
+
+  const driverFiles = files
+    .filter((file) => file.substr(-3) === '.js')
+    .filter((file) => file !== 'index.js')
+    .map((file) => file.replace('.js', ''));
+
+  expect(driverNames).toEqual(driverFiles);
+});
 
 describe('Drivers', () => {
   driverNames.forEach((driverName) => {
