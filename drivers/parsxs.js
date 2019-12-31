5e79b1d3e766b26b709333958093bc481146de19
Stella Sajcic <sajcic.stella@gmail.com>
2019-12-31 12:54:16 +0100

    feat: parsxc driver

---
+++ drivers/parsxs.js
@@ -0,0 +1,22 @@
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+module.exports = async () => {
+  const { data: { market: tickers } } = await request('https://parsxc.com/Api/Index/marketInfo');
+
+  return tickers.map((ticker) => {
+    const [base, quote] = ticker.ticker.split('_');
+
+    return new Ticker({
+      base,
+      quote,
+      high: parseToFloat(ticker.max_price),
+      low: parseToFloat(ticker.min_price),
+      close: parseToFloat(ticker.new_price),
+      bid: parseToFloat(ticker.buy_price),
+      ask: parseToFloat(ticker.sell_price),
+      baseVolume: parseToFloat(ticker.volume),
+    });
+  });
+};
