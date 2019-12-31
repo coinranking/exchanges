a7fa3fab0cf30960e1c1173004be69fdf6c0ed7d
Stella Sajcic <sajcic.stella@gmail.com>
2019-12-31 12:16:42 +0100

    feat: altmarkets driver

--- drivers/index.js
+++ drivers/index.js
@@ -4,6 +4,7 @@ exports.acx = require('./acx');
 exports.aex = require('./aex');
 exports.aidosmarket = require('./aidosmarket');
 exports.allcoin = require('./allcoin');
+exports.altmarkets = require('./altmarkets');
 exports.atomars = require('./atomars');
 exports.bancor = require('./bancor');
 exports.bancor = require('./bancor');
