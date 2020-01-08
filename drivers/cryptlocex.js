faebe70e64cb2ff4a3c3b0bcebe5cefaf5d77ee3
Stella Sajcic <sajcic.stella@gmail.com>
2020-01-08 18:00:25 +0100

    feat: cryptlocex driver

---
+++ drivers/cryptlocex.js
@@ -0,0 +1,27 @@
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+module.exports = async () => {
+  const {
+    data: { market: markets },
+  } = await request('https://trade.cryptlocex.com/Api/Index/marketInfo');
+
+  return markets.map((market) => {
+    const [base, quote] = market.ticker.split('_');
+    const ticker = market;
+    const [, baseName] = ticker.name.match(/(.*)\(.*\)/);
+
+    return new Ticker({
+      base,
+      baseName,
+      quote,
+      high: parseToFloat(ticker.max_price),
+      low: parseToFloat(ticker.min_price),
+      close: parseToFloat(ticker.new_price),
+      bid: parseToFloat(ticker.buy_price),
+      ask: parseToFloat(ticker.sell_price),
+      baseVolume: parseToFloat(ticker.volume),
+    });
+  });
+};
