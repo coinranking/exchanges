d18d2bceb903b25ffe273388ee18cbc8a1e814ea
Stella Sajcic <sajcic.stella@gmail.com>
2020-03-19 23:33:01 +0100

    fix: change url to fetch tickers

--- drivers/currency.js
+++ drivers/currency.js
@@ -3,17 +3,19 @@ const Ticker = require('../models/ticker');
 const { parseToFloat } = require('../lib/utils.js');
 
 module.exports = async () => {
-  const pairs = await request('https://marketcap.backend.currency.com/api/v1/ticker');
+  const tickers = await request(
+    'https://marketcap.backend.currency.com/api/v1/token_crypto/ticker',
+  );
 
-  return Object.keys(pairs).map((pair) => {
-    const ticker = pairs[pair];
-
-    return new Ticker({
-      base: ticker.base_currency,
-      quote: ticker.quote_currency,
-      close: parseToFloat(ticker.last_price),
-      baseVolume: parseToFloat(ticker.base_volume),
-      quoteVolume: parseToFloat(ticker.quote_volume),
-    });
-  });
+  return Object.values(tickers).map((ticker) => new Ticker({
+    base: ticker.base_currency,
+    quote: ticker.quote_currency,
+    high: parseFloat(ticker.past_24hrs_high_price),
+    low: parseFloat(ticker.past_24hrs_low_price),
+    close: parseToFloat(ticker.last_price),
+    bid: parseFloat(ticker.highest_bid_price),
+    ask: parseFloat(ticker.lowest_ask_price),
+    baseVolume: parseToFloat(ticker.base_volume),
+    quoteVolume: parseToFloat(ticker.quote_volume),
+  }));
 };
