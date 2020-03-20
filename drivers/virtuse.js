f5df62884dfbb17f0481e930d5b203e88b58210b
Stella <sajcic.stella@gmail.com>
2020-03-20 21:12:46 +0100

    fix: virtuse exchange with new logic of getting tickers

--- drivers/virtuse.js
+++ drivers/virtuse.js
@@ -1,53 +1,30 @@
 const request = require('../lib/request');
 const Ticker = require('../models/ticker');
-const { parseToFloat, throttleMap } = require('../lib/utils.js');
+const { parseToFloat } = require('../lib/utils.js');
 
-module.exports = async (isMocked) => {
-  const pairs = await request({
+module.exports = async () => {
+  const tickers = await request({
     url: 'https://virtuse.exchange/api/v1',
     method: 'post',
     form: {
-      method: 'public/get_pairs',
+      method: 'public/get_ticker',
     },
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
-  const tickers = throttleMap(
-    Object.keys(pairs),
-    async (pair) => {
-      try {
-        const data = await request({
-          url: 'https://virtuse.exchange/api/v1',
-          method: 'post',
-          form: {
-            method: 'public/get_ticker',
-            pair,
-          },
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+  return Object.values(tickers).map((ticker) => {
+    const { base: baseVolume, quote: quoteVolume } = ticker.volume;
 
-        const ticker = data[pair];
-        const { base: baseVolume, quote: quoteVolume } = ticker.volume;
-
-        return new Ticker({
-          base: ticker.base,
-          quote: ticker.quote,
-          high: parseToFloat(ticker.high),
-          low: parseToFloat(ticker.low),
-          close: parseToFloat(ticker.last),
-          baseVolume: parseToFloat(baseVolume),
-          quoteVolume: parseToFloat(quoteVolume),
-        });
-      } catch (error) {
-        return undefined;
-      }
-    },
-    isMocked ? 0 : 50,
-  ); // 20 requests per second
-
-  return Promise.all(tickers);
+    return new Ticker({
+      base: ticker.base,
+      quote: ticker.quote,
+      high: parseToFloat(ticker.high),
+      low: parseToFloat(ticker.low),
+      close: parseToFloat(ticker.last),
+      baseVolume: parseToFloat(baseVolume),
+      quoteVolume: parseToFloat(quoteVolume),
+    });
+  });
 };
