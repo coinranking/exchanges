36fb07dbbb4c3660ee748f6ec7c3db360c7c1845
Abdul Qabiz <abdul.qabiz@gmail.com>
2020-03-25 01:17:28 +0530

    feat: drgx driver

---
+++ drivers/drgx.js
@@ -0,0 +1,23 @@
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+module.exports = async () => {
+  const tickers = await request('https://exchange.drgx.io/cmc/public/returnTicker');
+  const pairs = Object.keys(tickers);
+
+  return pairs.map((pair) => {
+    const [base, quote] = pair.split('-');
+    const ticker = tickers[pair];
+
+    return new Ticker({
+      base,
+      quote,
+      quoteVolume: parseToFloat(ticker.quoteVolume24hr),
+      baseVolume: parseToFloat(ticker.volume24hr),
+      close: parseToFloat(ticker.last),
+      high: parseToFloat(ticker.high24hr),
+      low: parseToFloat(ticker.low24hr),
+    });
+  });
+};
