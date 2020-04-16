72a02e65c093243580ed7f94f24dca058e2dd992
Stella <sajcic.stella@gmail.com>
2020-04-16 18:10:57 +0200

    feat: walkex driver

---
+++ drivers/walkex.js
@@ -0,0 +1,21 @@
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+module.exports = async () => {
+  const { data } = await request('https://backend.walkex.com/api/tickers');
+
+  return Object.keys(data).map((pair) => {
+    const ticker = data[pair];
+    const [base, quote] = ticker.pair.split('_');
+
+    return new Ticker({
+      base,
+      quote,
+      close: parseToFloat(ticker.last),
+      ask: parseToFloat(ticker.highestBid), // reversed with ask
+      bid: parseToFloat(ticker.lowestAsk),
+      baseVolume: parseToFloat(ticker.volume24hr),
+    });
+  });
+};
