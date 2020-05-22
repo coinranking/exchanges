52884fa9c97f183c5395676c616a8926c271f5e1
nickpater <nick@coinranking.com>
2020-05-22 16:12:10 +0200

    fix: fix the bcbitcoin test, because it failed after 24 hours

--- drivers/bcbitcoin.js
+++ drivers/bcbitcoin.js
@@ -4,29 +4,22 @@ const { parseToFloat } = require('../lib/utils.js');
 
 module.exports = async () => {
   const markets = await request('https://www.bcbitcoin.co.uk/api/products/');
-  const { Trades: trades } = await request('https://www.bcbitcoin.co.uk/api/trades/');
+  const { Trades: trades } = await request('https://www.bcbitcoin.co.uk/api/trades/?today=24');
 
   return markets.map((market) => {
     const [base, quote] = market.id.split('-');
 
-    const twentyFourHoursAgoInSeconds = (Date.now() / 1000) - (24 * 60 * 60);
     const tradesInLast24h = trades
-      .filter((trade) => trade.pair === market.id)
-      .filter((trade) => trade.timestamp >= twentyFourHoursAgoInSeconds);
+      .filter((trade) => trade.pair === market.id);
 
-    let high;
-    let low;
-    let close;
-    let baseVolume;
+    if (!tradesInLast24h.length) return undefined;
 
-    if (tradesInLast24h.length) {
-      high = tradesInLast24h
-        .reduce((acc, value) => Math.max(acc, parseToFloat(value.price)), -Infinity);
-      low = tradesInLast24h
-        .reduce((acc, value) => Math.min(acc, parseToFloat(value.price)), Infinity);
-      close = parseToFloat(tradesInLast24h[tradesInLast24h.length - 1].price);
-      baseVolume = tradesInLast24h.reduce((acc, value) => acc + parseToFloat(value.amount), 0);
-    }
+    const high = tradesInLast24h
+      .reduce((acc, value) => Math.max(acc, parseToFloat(value.price)), -Infinity);
+    const low = tradesInLast24h
+      .reduce((acc, value) => Math.min(acc, parseToFloat(value.price)), Infinity);
+    const close = parseToFloat(tradesInLast24h[tradesInLast24h.length - 1].price);
+    const baseVolume = tradesInLast24h.reduce((acc, value) => acc + parseToFloat(value.amount), 0);
 
     return new Ticker({
       base,
