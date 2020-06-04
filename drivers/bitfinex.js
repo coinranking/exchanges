f4da67a5d6b5199c5d62e6261ec42b0fb50266b5
nickpater <nick@coinranking.com>
2020-06-04 17:10:13 +0200

    feat(bitfinex): map the real world symbol, fetch the name

--- drivers/bitfinex.js
+++ drivers/bitfinex.js
@@ -14,15 +14,23 @@ class Bitfinex extends Driver {
    */
   async fetchTickers() {
     const tickers = await request('https://api.bitfinex.com/v1/tickers');
+    const [names, symbols] = await request('https://api-pub.bitfinex.com/v2/conf/pub:map:currency:label,pub:map:currency:sym');
+    const [currencies] = await request('https://api-pub.bitfinex.com/v2/conf/pub:list:currency');
+
+    const namesMap = new Map(names);
+    const symbolsMap = new Map(symbols);
 
     return tickers.map((ticker) => {
-      const pair = /^([A-Z]*)(USD|BTC|EUR|JPY|ETH|GBP|DAI|EOS|XLM)$/.exec(ticker.pair);
+      const regex = new RegExp(`^([A-Z]*)(${currencies.join('|')})$`);
+      const pair = regex.exec(ticker.pair);
       if (!pair) return undefined;
       const [, base, quote] = pair;
 
       return new Ticker({
-        base,
-        quote,
+        base: symbolsMap.get(base) || base,
+        quote: symbolsMap.get(quote) || quote,
+        baseName: namesMap.get(base),
+        quoteName: namesMap.get(quote),
         baseVolume: parseToFloat(ticker.volume),
         close: parseToFloat(ticker.last_price),
         high: parseToFloat(ticker.high),
