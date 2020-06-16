a886dc87a5ff1210732100e9566dda589e12e6e8
Stella <sajcic.stella@gmail.com>
2020-06-16 23:06:43 +0200

    feat: cryptocore driver

---
+++ drivers/cryptocore.js
@@ -0,0 +1,37 @@
+const Driver = require('../models/driver');
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+/**
+ * @memberof Driver
+ * @augments Driver
+ */
+class Cryptocore extends Driver {
+  /**
+   * @augments Driver.fetchTickers
+   * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
+   */
+  async fetchTickers() {
+    const { data: pairs } = await request('https://crypcore.com/api/v1/summary');
+
+    return Object.keys(pairs).map((pair) => {
+      const ticker = pairs[pair];
+      const [base, quote] = pair.split('_');
+
+      return new Ticker({
+        base,
+        quote,
+        high: parseToFloat(ticker.high24hr),
+        low: parseToFloat(ticker.low24hr),
+        close: parseToFloat(ticker.last),
+        bid: parseToFloat(ticker.highestBid),
+        ask: parseToFloat(ticker.lowestAsk),
+        baseVolume: parseToFloat(ticker.baseVolume),
+        quoteVolume: parseToFloat(ticker.quoteVolume),
+      });
+    });
+  }
+}
+
+module.exports = Cryptocore;
