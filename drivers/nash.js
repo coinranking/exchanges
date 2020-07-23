77fb226de169000aada8d3d733a64b6870439088
Stella <sajcic.stella@gmail.com>
2020-07-23 12:49:22 +0200

    feat: fetch data from graphql

--- drivers/nash.js
+++ drivers/nash.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const Driver = require('../models/driver');
 const request = require('../lib/request');
 const Ticker = require('../models/ticker');
@@ -13,18 +14,59 @@ class Nash extends Driver {
    * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
    */
   async fetchTickers() {
-    const markets = await request('https://app.nash.io/api/cmc/ticker');
+    const { data: { listTickers: tickers } } = await request({
+      method: 'POST',
+      url: 'https://app.nash.io/api/graphql',
+      json: {
+        query: `
+          query Ticker {
+            listTickers {
+              id,
+              volume_24h {amount}
+              bestAskPrice {amount},
+              bestBidPrice {amount},
+              lastPrice {amount},
+              highPrice_24h {amount},
+              lowPrice_24h {amount},
+              market {
+                aUnit,
+                aAsset {name},
+                bAsset {name},
+                bUnit
+              }
+            }
+          }
+        `,
+      },
+    });
 
-    return Object.keys(markets).map((market) => {
-      const [base, quote] = market.split('_');
-      const ticker = markets[market];
+    return tickers.map((ticker) => {
+      const {
+        market: {
+          aUnit: baseName,
+          aAsset: { name: base },
+          bUnit: quoteName,
+          bAsset: { name: quote },
+        },
+        bestBidPrice,
+        bestAskPrice,
+        lastPrice,
+        highPrice_24h,
+        lowPrice_24h,
+        volume_24h,
+      } = ticker;
 
       return new Ticker({
         base,
+        baseName,
         quote,
-        close: parseToFloat(ticker.last_price),
-        baseVolume: parseToFloat(ticker.base_volume),
-        quoteVolume: parseToFloat(ticker.quote_volume),
+        quoteName,
+        bid: parseToFloat(bestBidPrice ? bestBidPrice.amount : null),
+        ask: parseToFloat(bestAskPrice ? bestAskPrice.amount : null),
+        close: parseToFloat(lastPrice ? lastPrice.amount : null),
+        high: parseToFloat(highPrice_24h ? highPrice_24h.amount : null),
+        low: parseToFloat(lowPrice_24h ? lowPrice_24h.amount : null),
+        baseVolume: parseToFloat(volume_24h ? volume_24h.amount : null),
       });
     });
   }
