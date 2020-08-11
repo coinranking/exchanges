dd2dc3b310937e0577011054645ffae108414ea1
Nick Pater <nick@coinranking.com>
2020-08-11 15:02:23 +0200

    feat(polarity): use the waves driver
    Use the Waves driver because it uses the same API structure and because 
    this one supports more features like the references

--- drivers/polarity.js
+++ drivers/polarity.js
@@ -13,46 +13,67 @@ class Polarity extends Driver {
    * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
    */
   async fetchTickers() {
-    const { data: tickers } = await request(
-      'https://data-service.polarity.exchange/pairs',
-    );
-    const { data: symbols } = await request('https://data-service.polarity.exchange/assets?ticker=%2A&limit=100');
-    const markets = {
-      '7RB3BWayeCVPq3kkpkeJZAFv2DYCB5gEwnutEpRofaw4': {
-        symbol: 'USDT',
-        name: 'Tether',
-      },
-    };
+    const { data: markets } = await request('https://data-service.polarity.exchange/pairs?limit=1000');
+    const { data: assets } = await request('https://data-service.polarity.exchange/assets?ticker=*');
 
-    symbols.forEach((el) => {
-      const { data } = el;
+    const currencies = assets.map((asset) => ({
+      symbol: asset.data.ticker.trim(),
+      name: asset.data.name,
+      address: asset.data.id,
+    }));
 
-      markets[data.id] = {
-        symbol: data.ticker,
-        name: data.name,
-      };
+    // Add Tether manually because polarity.exchange
+    // didn't include the quote currency in the assets list
+    currencies.push({
+      address: '7RB3BWayeCVPq3kkpkeJZAFv2DYCB5gEwnutEpRofaw4',
+      symbol: 'USDT',
+      name: 'Tether',
     });
 
-    return tickers.map((ticker) => {
-      if (!markets[ticker.amountAsset] || !markets[ticker.priceAsset]) {
-        return undefined;
+    return markets.map((market) => {
+      const baseCurrency = currencies.find((currency) => (market.amountAsset === currency.address));
+      const quoteCurrency = currencies.find((currency) => (market.priceAsset === currency.address));
+
+      let base;
+      let baseName;
+      let baseReference;
+      if (baseCurrency) {
+        base = baseCurrency.symbol;
+        baseName = baseCurrency.name;
+        baseReference = baseCurrency.address;
+      } else {
+        base = market.amountAsset;
+        baseReference = market.amountAsset;
       }
 
-      const { symbol: base, name: baseName } = markets[ticker.amountAsset];
-      const { symbol: quote, name: quoteName } = markets[ticker.priceAsset];
+      let quote;
+      let quoteName;
+      let quoteReference;
+      if (quoteCurrency) {
+        quote = quoteCurrency.symbol;
+        quoteName = quoteCurrency.name;
+        quoteReference = quoteCurrency.address;
+      } else {
+        quote = market.priceAsset;
+        quoteReference = market.priceAsset;
+      }
 
-      const { data } = ticker;
+      const ticker = market.data;
 
       return new Ticker({
         base,
         baseName,
+        baseReference,
         quote,
         quoteName,
-        high: parseToFloat(data.high),
-        low: parseToFloat(data.low),
-        close: parseToFloat(data.lastPrice),
-        baseVolume: parseToFloat(data.volume),
-        quoteVolume: parseToFloat(data.quoteVolume),
+        quoteReference,
+        open: parseToFloat(ticker.firstPrice),
+        high: parseToFloat(ticker.high),
+        low: parseToFloat(ticker.low),
+        close: parseToFloat(ticker.lastPrice),
+        vwap: parseToFloat(ticker.weightedAveragePrice),
+        baseVolume: parseToFloat(ticker.volume),
+        quoteVolume: parseToFloat(ticker.quoteVolume),
       });
     });
   }
