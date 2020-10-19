b0c59d7253575a497a1d01400b283077dc96ac07
Stella <sajcic.stella@gmail.com>
2020-10-19 13:19:44 +0200

    feat: crypxie driver

---
+++ drivers/crypxie.js
@@ -0,0 +1,36 @@
+const Driver = require('../models/driver');
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils.js');
+
+/**
+ * @memberof Driver
+ * @augments Driver
+ */
+class Crypxie extends Driver {
+  /**
+   * @augments Driver.fetchTickers
+   * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
+   */
+  async fetchTickers() {
+    const { result: tickers } = await request('https://crypxie.com/api/v1/tickers');
+
+    return tickers.map((ticker) => {
+      const [quote, base] = ticker.ticker_id.split('_');
+
+      return new Ticker({
+        base,
+        quote,
+        high: parseToFloat(ticker.high),
+        low: parseToFloat(ticker.low),
+        close: parseToFloat(ticker.last_price),
+        bid: parseToFloat(ticker.bid),
+        ask: parseToFloat(ticker.ask),
+        baseVolume: parseToFloat(ticker.target_volume),
+        quoteVolume: parseToFloat(ticker.base_volume),
+      });
+    });
+  }
+}
+
+module.exports = Crypxie;
