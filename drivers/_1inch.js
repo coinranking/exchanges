0fa2bdaff5bb9f0d8f4cf49c0cc52adc53df5e9f
Riccardo Parrello <riccardo@coinranking.com>
2021-06-03 14:07:44 +0200

    fix: fix 1inch driver issues
    Corrected base and quote and added checks to make sure
    we don't do calculations with zeroes as otherwise we might
    get some weird values, for example there were some tickers
    with the close being Infinity.

--- drivers/_1inch.js
+++ drivers/_1inch.js
@@ -14,15 +14,25 @@ class _1inch extends Driver {
   async fetchTickers() {
     const tickers = await request('https://governance.1inch.exchange/v1.1/protocol/pairs');
 
-    return tickers.map((ticker) => new Ticker({
-      base: ticker.token0.symbol,
-      baseReference: ticker.token0.address,
-      quote: ticker.token1.symbol,
-      quoteReference: ticker.token1.address,
-      close: ticker.token0.derivedETH / ticker.token1.derivedETH,
-      baseVolume: ticker.volumeUSD24h / (ticker.reserveUSD / 2 / ticker.reserve0),
-      quoteVolume: ticker.volumeUSD24h / (ticker.reserveUSD / 2 / ticker.reserve1),
-    }));
+    return tickers.map((ticker) => {
+      if (
+        Number(ticker.token0.derivedETH) === 0
+        || Number(ticker.token1.derivedETH) === 0
+        || Number(ticker.reserveUSD) === 0
+        || Number(ticker.reserve0) === 0
+        || Number(ticker.reserve1) === 0
+      ) return undefined;
+
+      return new Ticker({
+        base: ticker.token1.symbol,
+        baseReference: ticker.token1.address,
+        quote: ticker.token0.symbol,
+        quoteReference: ticker.token0.address,
+        close: ticker.token1.derivedETH / ticker.token0.derivedETH,
+        baseVolume: ticker.volumeUSD24h / (ticker.reserveUSD / 2 / ticker.reserve1),
+        quoteVolume: ticker.volumeUSD24h / (ticker.reserveUSD / 2 / ticker.reserve0),
+      });
+    });
   }
 }
 
