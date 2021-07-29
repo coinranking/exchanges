80d45e409e2729e54a5cd264849b97a31545593b
Matúš S <matusstaviarsky@gmail.com>
2021-07-29 13:00:31 +0200

    Add Exbitron Exchange

---
+++ drivers/exbitron.js
@@ -0,0 +1,34 @@
+const Driver = require('../models/driver');
+const request = require('../lib/request');
+const Ticker = require('../models/ticker');
+const { parseToFloat } = require('../lib/utils');
+
+/**
+ * @memberof Driver
+ * @augments Driver
+ */
+class Exbitron extends Driver {
+  /**
+   * @augments Driver.fetchTickers
+   * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
+   */
+  async fetchTickers() {
+    const { data: tickers } = await request('https://www.exbitron.com/api/v2/peatio/coinmarketcap/summary');
+
+    return tickers.map((ticker) => {
+      const [base, quote] = ticker.trading_pairs.split('-');
+
+      return new Ticker({
+        base,
+        quote,
+        high: parseToFloat(ticker.highest_price_24h),
+        low: parseToFloat(ticker.lowest_price_24h),
+        close: parseToFloat(ticker.last_price),
+        baseVolume: parseToFloat(ticker.base_volume),
+        quoteVolume: parseToFloat(ticker.quote_volume),
+      });
+    });
+  }
+}
+
+module.exports = Exbitron;
