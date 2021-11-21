c4436d22b5477e697f6ec3bfd08df5c6a28793a0
Matúš <matusstaviarsky@gmail.com>
2021-11-21 17:39:22 +0100

    Update exbitron.js

--- drivers/exbitron.js
+++ drivers/exbitron.js
@@ -13,22 +13,25 @@ class Exbitron extends Driver {
    * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
    */
   async fetchTickers() {
-    const tickers = await request('https://www.exbitron.com/api/v2/peatio/coingecko/tickers');
+    const { data: { markets: tickers } } = await request('https://www.exbitron.com/api/v2/peatio/coingecko/tickers');
 
     return tickers.map((ticker) => {
       const [base, quote] = ticker.ticker_id.split('_');
 
       return new Ticker({
-      base: ticker.base_currency,
-      quote: ticker.target_currency,
-      high: parseToFloat(ticker.high),
-      low: parseToFloat(ticker.low),
-      close: parseToFloat(ticker.last_price),
-      bid: parseToFloat(ticker.bid),
-      ask: parseToFloat(ticker.ask),
-      baseVolume: parseToFloat(ticker.base_volume),
-      quoteVolume: parseToFloat(ticker.target_volume),
-    }));
+        base,
+        quote,
+        base: ticker.base_currency,
+        quote: ticker.target_currency,
+        high: parseToFloat(ticker.high),
+        low: parseToFloat(ticker.low),
+        close: parseToFloat(ticker.last_price),
+        bid: parseToFloat(ticker.bid),
+        ask: parseToFloat(ticker.ask),
+        baseVolume: parseToFloat(ticker.base_volume),
+        quoteVolume: parseToFloat(ticker.target_volume),
+      });
+    });
   }
 }
 
