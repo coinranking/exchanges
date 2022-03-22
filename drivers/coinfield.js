7317b1d171999945d3d3748bb9bed10bb203f53c
Wout Hoekstra <whoekstra@mixcom.nl>
2022-03-22 15:03:11 +0100

    fix: coinfield driver was not working for reasons unknown but it could do without a request per market

--- drivers/coinfield.js
+++ drivers/coinfield.js
@@ -1,7 +1,7 @@
 const Driver = require('../models/driver');
 const request = require('../lib/request');
 const Ticker = require('../models/ticker');
-const { throttleMap, parseToFloat } = require('../lib/utils');
+const { parseToFloat } = require('../lib/utils');
 
 /**
  * @memberof Driver
@@ -9,33 +9,28 @@ const { throttleMap, parseToFloat } = require('../lib/utils');
  */
 class Coinfield extends Driver {
   /**
-   * @param {boolean} isMocked Set to true when stored tickers are used
    * @augments Driver.fetchTickers
    * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
    */
-  async fetchTickers(isMocked) {
-    const { markets: pairs } = await request('https://api.coinfield.com/v1/markets');
-    const symbols = pairs.map((item) => item.name);
+  async fetchTickers() {
+    const { markets } = await request('https://api.coinfield.com/v1/markets');
+    const { tickers } = await request('https://api.coinfield.com/v1/tickers');
 
-    const tickers = throttleMap(symbols, async (symbol) => {
-      const [base, quote] = symbol.split('/');
-      const tickerName = `${base.toLowerCase()}${quote.toLowerCase()}`;
-      const { markets } = await request(`https://api.coinfield.com/v1/tickers/${tickerName}`);
-      const ticker = markets[0];
+    return markets.map((market) => {
+      const ticker = tickers[market.id];
 
       return new Ticker({
-        base,
-        quote,
+        base: market.ask_unit,
+        quote: market.bid_unit,
+        open: parseToFloat(ticker.open),
         high: parseToFloat(ticker.high),
         low: parseToFloat(ticker.low),
         close: parseToFloat(ticker.last),
         bid: parseToFloat(ticker.bid),
         ask: parseToFloat(ticker.ask),
-        baseVolume: parseToFloat(ticker.vol),
+        baseVolume: parseToFloat(ticker.volume),
       });
-    }, isMocked ? 0 : 20); // 20 requests per second
-
-    return Promise.all(tickers);
+    });
   }
 }
 
