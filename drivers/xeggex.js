5f6376b153bac0161c35fef0c861ebfe7c2e6288
Xeggex <support@xeggex.com>
2022-09-07 11:58:34 +0000

    fix: lint

--- drivers/xeggex.js
+++ drivers/xeggex.js
@@ -16,8 +16,7 @@ class Xeggex extends Driver {
     const data = await request('https://xeggex.com/api/v2/tickers');
     // Return the data mapped to instances of the Ticker model, the exact way will differ for every
     // exchange.
-    return data.map((item) => 
-      new Ticker({
+    return data.map((item) => new Ticker({
         base: item.base_currency,
         quote: item.target_currency,
         quoteVolume: parseToFloat(item.target_volume),
@@ -27,8 +26,7 @@ class Xeggex extends Driver {
         low: parseToFloat(item.low),
         ask: parseToFloat(item.ask),
         bid: parseToFloat(item.bid),
-      })
-    );
+    }));
   }
 }
 
