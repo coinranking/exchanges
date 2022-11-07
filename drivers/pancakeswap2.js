111a3b64c6b19e0965ce19a5cee7fef2eb7c16d9
wouthoekstra <wouthoekstra@gmail.com>
2022-11-07 15:25:09 +0100

    fix: migrate pancakeswap v2 to bitquery api

--- drivers/pancakeswap2.js
+++ drivers/pancakeswap2.js
@@ -8,28 +8,79 @@ const { parseToFloat } = require('../lib/utils');
  * @augments Driver
  */
 class Pancakeswap2 extends Driver {
+  constructor() {
+    super({
+      requires: {
+        key: true,
+      },
+    });
+  }
+
   /**
    * @augments Driver.fetchTickers
    * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
    */
   async fetchTickers() {
-    const { data: tickers } = await request('https://api.pancakeswap.info/api/v2/pairs');
+    const date = new Date();
+    const current = date.toISOString();
+    const twentyFourOursAgo = (new Date(date - (24 * 3600 * 1000))).toISOString();
+    const minimumVolumeInUsd = 5000;
 
-    return Object.keys(tickers).map((key) => {
-      const ticker = tickers[key];
-
-      return new Ticker({
-        base: ticker.base_symbol,
-        baseName: ticker.base_name,
-        baseReference: ticker.base_address,
-        quote: ticker.quote_symbol,
-        quoteName: ticker.quote_name,
-        quoteReference: ticker.quote_address,
-        close: parseToFloat(ticker.price),
-        baseVolume: parseToFloat(ticker.base_volume),
-        quoteVolume: parseToFloat(ticker.quote_volume),
-      });
+    const result = await request({
+      method: 'POST',
+      url: 'https://graphql.bitquery.io',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': this.key,
+      },
+      json: {
+        query: `
+          {
+            exchange: ethereum(network: bsc) {
+              tickers: dexTrades(
+                exchangeName: {in: ["Pancake v2"]}
+                tradeAmountUsd: {gt: ${minimumVolumeInUsd}}
+                time: {since: "${twentyFourOursAgo}", till: "${current}"}
+              ) {
+                baseCurrency {
+                  symbol
+                  address
+                  name
+                }
+                quoteCurrency {
+                  symbol
+                  address
+                  name
+                }
+                baseVolume: baseAmount
+                quoteVolume: quoteAmount
+                open: minimum(of: time, get: quote_price)
+                high: maximum(of: quote_price, get: quote_price)
+                low: minimum(of: quote_price, get: quote_price)
+                close: maximum(of: time, get: quote_price)
+              }
+            }
+          }
+        `,
+      },
     });
+
+    const tickers = result.data.exchange.tickers.map((ticker) => ({
+      base: ticker.baseCurrency.symbol,
+      baseName: ticker.baseCurrency.name,
+      baseReference: ticker.baseCurrency.address,
+      quote: ticker.quoteCurrency.symbol,
+      quoteName: ticker.quoteCurrency.name,
+      quoteReference: ticker.quoteCurrency.address,
+      baseVolume: parseToFloat(ticker.baseVolume),
+      quoteVolume: parseToFloat(ticker.quoteVolume),
+      open: parseToFloat(ticker.open),
+      high: parseToFloat(ticker.high),
+      low: parseToFloat(ticker.low),
+      close: parseToFloat(ticker.close),
+    }));
+
+    return tickers;
   }
 }
 
