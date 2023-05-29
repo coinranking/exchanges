92cd4720ef134f3bd8727ad8aa4a45a9203147b2
Nick Pater <nick@coinranking.com>
2023-05-29 13:57:39 +0800

    fix: replace the BTCAlpha API

--- drivers/btcalpha.js
+++ drivers/btcalpha.js
@@ -1,41 +1,28 @@
 const Driver = require('../models/driver');
 const request = require('../lib/request');
 const Ticker = require('../models/ticker');
-const { throttleMap, parseToFloat } = require('../lib/utils');
+const { parseToFloat } = require('../lib/utils');
 
 /**
  * @memberof Driver
  * @augments Driver
  */
 class Btcalpha extends Driver {
-  /**
-   * @param {boolean} isMocked Set to true when stored tickers are used
-   * @augments Driver.fetchTickers
-   * @returns {Promise.Array<Ticker>} Returns a promise of an array with tickers.
-   */
-  async fetchTickers(isMocked) {
-    const markets = await request('https://btc-alpha.com/api/v1/pairs/');
+  async fetchTickers() {
+    const tickers = await request('https://btc-alpha.com/api/v1/ticker/');
 
-    const tickers = throttleMap(markets, async (market) => {
-      const [ticker] = await request(`https://btc-alpha.com/api/charts/${market.name}/D/chart/?limit=1`);
-
-      if (!ticker) return undefined;
-
-      const base = market.currency1;
-      const quote = market.currency2;
+    return tickers.map((ticker) => {
+      const [base, quote] = ticker.pair.split('_');
 
       return new Ticker({
         base,
         quote,
-        baseVolume: parseToFloat(ticker.volume),
-        open: parseToFloat(ticker.open),
+        baseVolume: parseToFloat(ticker.vol),
         high: parseToFloat(ticker.high),
         low: parseToFloat(ticker.low),
-        close: parseToFloat(ticker.close),
+        close: parseToFloat(ticker.last),
       });
-    }, isMocked ? 0 : 200); // 5 requests per second
-
-    return Promise.all(tickers);
+    });
   }
 }
 
