ab824b407cb5b0bc65e3b2164bdb7ffcd4d8afe4
wouthoekstra <wouthoekstra@gmail.com>
2023-12-04 11:44:20 +0100

    feat: add pancakeswap 3 bsc

--- drivers/index.js
+++ drivers/index.js
@@ -160,6 +160,7 @@ exports.Orebz = require('./orebz');
 exports.Osmosis = require('./osmosis');
 exports.P2pb2b = require('./p2pb2b');
 exports.Pancakeswap2 = require('./pancakeswap2');
+exports.Pancakeswap3bsc = require('./pancakeswap3bsc');
 exports.Paribu = require('./paribu');
 exports.Parsxc = require('./parsxc');
 exports.Paycml = require('./paycml');
